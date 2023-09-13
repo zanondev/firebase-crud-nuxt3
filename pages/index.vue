2f49adff2cd09e4a087c8c5097ab452c3e083562
Lucas <szanonn@gmail.com>
2023-09-13 16:50:31 -0300

    FEAT: Deleting

--- pages/index.vue
+++ pages/index.vue
@@ -7,17 +7,41 @@
       </div>
       <div>
         <label for="password">Password:</label>
-        <input type="password" id="password" v-model="customer.password" required />
+        <input
+          type="password"
+          id="password"
+          v-model="customer.password"
+          required
+        />
       </div>
       <div>
         <button type="submit">Save</button>
       </div>
     </form>
+
+    <h2>Lista de Customers</h2>
+    <table>
+      <thead>
+        <tr>
+          <th>Email</th>
+          <th>Password</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="customer in customers" :key="customer.id">
+          <td>{{ customer.email }}</td>
+          <td>{{ customer.password }}</td>
+          <td>
+            <button @click="removeCustomer(customer.id)">Delete</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-import { getFirestore, collection, addDoc } from "firebase/firestore";
+import { getFirestore, collection, addDoc, getDocs, doc, deleteDoc } from "firebase/firestore";
 
 export default {
   data() {
@@ -26,7 +50,11 @@ export default {
         email: "",
         password: "",
       },
+      customers: [],
     };
+  },
+  mounted() {
+    this.loadCustomers(); // Carregue os customers quando o componente for montado
   },
   methods: {
     async saveCustomer() {
@@ -46,8 +74,43 @@ export default {
         this.customer.password = "";
 
         alert("Customer saved successfully!");
+
+        this.$router.go(0);
       } catch (error) {
         console.error("Error saving customer:", error);
+      }
+    },
+
+    async loadCustomers() {
+      try {
+        const firestore = getFirestore();
+        const customersCollection = collection(firestore, "customers");
+        const querySnapshot = await getDocs(customersCollection);
+
+        // Mapeie os documentos da coleção para o array customers
+        this.customers = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+      } catch (error) {
+        console.error("Error loading customers:", error);
+      }
+    },
+
+    async removeCustomer(customerId) {
+      try {
+        const firestore = getFirestore();
+        const customerRef = doc(firestore, "customers", customerId);
+
+        // Exclua o cliente com base no ID
+        await deleteDoc(customerRef);
+
+        // Recarregue a lista após a remoção
+        await this.loadCustomers();
+
+        alert("Customer removed successfully!");
+      } catch (error) {
+        console.error("Error removing customer:", error);
       }
     },
   },
